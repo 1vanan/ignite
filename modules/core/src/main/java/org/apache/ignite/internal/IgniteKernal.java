458cd8c41623e47cdb4650a65c16c327bb627e14
Ivan <vanen31@gmail.com>
2017-11-08 13:53:51 +0300

    initial refactoring

--- modules/core/src/main/java/org/apache/ignite/internal/IgniteKernal.java
+++ modules/core/src/main/java/org/apache/ignite/internal/IgniteKernal.java
@@ -285,6 +285,9 @@ public class IgniteKernal implements IgniteEx, IgniteMXBean, Externalizable {
     /** Configuration. */
     private IgniteConfiguration cfg;
 
+    /** Various information on output. */
+    OutputVariousInformation variousInformation;
+
     /** */
     @SuppressWarnings({"FieldAccessedSynchronizedAndUnsynchronized"})
     @GridToStringExclude
@@ -802,6 +805,8 @@ public class IgniteKernal implements IgniteEx, IgniteMXBean, Externalizable {
             getClass().getName() + (igniteInstanceName != null ? '%' + igniteInstanceName : ""));
 
         RuntimeMXBean rtBean = ManagementFactory.getRuntimeMXBean();
+
+        variousInformation = new OutputVariousInformation(log, cfg);
 
         // Ack various information.
         ackAsciiLogo();
