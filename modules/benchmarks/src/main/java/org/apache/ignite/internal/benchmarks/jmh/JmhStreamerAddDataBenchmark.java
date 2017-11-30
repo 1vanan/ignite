914bcac4f94e68962fd7c5523fda4a2d1a06fd63
Ivan <vanen31@gmail.com>
2017-11-30 15:08:14 +0300

    benchmark on this case was developed

---
+++ modules/benchmarks/src/main/java/org/apache/ignite/internal/benchmarks/jmh/JmhStreamerAddDataBenchmark.java
@@ -0,0 +1,165 @@
+package org.apache.ignite.internal.benchmarks.jmh;
+
+import org.apache.ignite.Ignite;
+import org.apache.ignite.IgniteDataStreamer;
+import org.apache.ignite.Ignition;
+import org.apache.ignite.configuration.CacheConfiguration;
+import org.apache.ignite.configuration.IgniteConfiguration;
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.ChainedOptionsBuilder;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
+import static org.apache.ignite.cache.CacheAtomicityMode.TRANSACTIONAL;
+import static org.apache.ignite.cache.CacheWriteSynchronizationMode.FULL_SYNC;
+
+@State(Scope.Benchmark)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+public class JmhStreamerAddDataBenchmark extends JmhAbstractBenchmark {
+    /**
+     * Default cache name.
+     */
+    private static final String DEFAULT_CACHE_NAME = "default";
+
+    /**
+     * Data loader.
+     */
+    private IgniteDataStreamer<Integer, Integer> dataLdr;
+
+    /**
+     * Test list.
+     */
+    private static Collection<AbstractMap.SimpleEntry<Integer, Integer>> testList = new ArrayList<>();
+
+    /**
+     * Data amount.
+     */
+    private static final int DATA_AMOUNT = 1000;
+
+    /** Client. */
+    private Ignite srv1;
+
+    /** Server 2. */
+    private Ignite srv2;
+
+
+    /**
+     * Create Ignite configuration.
+     */
+    private static IgniteConfiguration getConfiguration(String cfgName, boolean isClient) {
+        IgniteConfiguration cfg = new IgniteConfiguration();
+        cfg.setIgniteInstanceName(cfgName);
+
+        cfg.setCacheConfiguration(defaultCacheConfiguration());
+
+        if (isClient)
+            cfg.setClientMode(true);
+
+        return cfg;
+    }
+
+    /**
+     * @return New cache configuration with modified defaults.
+     */
+    private static CacheConfiguration defaultCacheConfiguration() {
+        CacheConfiguration cfg = new CacheConfiguration(DEFAULT_CACHE_NAME);
+        cfg.setAtomicityMode(TRANSACTIONAL);
+        cfg.setWriteSynchronizationMode(FULL_SYNC);
+
+        return cfg;
+    }
+
+    @State(Scope.Benchmark)
+    public static class StreamingMap{
+        static Map<Integer, Integer> intMap = new HashMap<>();
+    }
+
+
+    /**
+     * Start 3 servers and 1 client.
+     */
+    @Setup(Level.Trial)
+    public void setup() {
+        srv1 = Ignition.start(getConfiguration("srv1", false));
+        srv2 = Ignition.start(getConfiguration("srv2", false));
+        Ignite client = Ignition.start(getConfiguration("clt", true));
+
+        prepareStreamingCollection();
+
+        dataLdr = client.dataStreamer(DEFAULT_CACHE_NAME);
+
+        for(int i = 0; i < DATA_AMOUNT; i++)
+            StreamingMap.intMap.put(i, i);
+    }
+
+    /**
+     *Fill streaming collection.
+     */
+    private static void prepareStreamingCollection() {
+        for (int i = 0; i < DATA_AMOUNT; i++)
+            testList.add(new HashMap.SimpleEntry<>(i, i));
+    }
+
+    /**
+     * Stop all grids after all tests.
+     */
+    @TearDown(Level.Trial)
+    public void tearDown(){
+        Ignition.stopAll(true);
+    }
+
+    /**
+     * Clean caches and loader after each operation.
+     */
+    @TearDown(Level.Iteration)
+    public void clean(){
+        srv1.cache(DEFAULT_CACHE_NAME).clear();
+        srv2.cache(DEFAULT_CACHE_NAME).clear();
+
+        dataLdr.flush();
+    }
+
+    /**
+     * Perfomance of addData per collection.
+     */
+    @Benchmark
+    public void addDataCollection() {
+        dataLdr.addData(testList);
+    }
+
+    /**
+     * Perfomance of addData per key value.
+     */
+    @Benchmark
+    public void addDataKeyValue(StreamingMap streamingMap) {
+
+        Map<Integer, Integer> data = streamingMap.intMap;
+
+        for (Map.Entry<Integer, Integer> entry : data.entrySet())
+            dataLdr.addData(entry.getKey(), entry.getValue());
+
+    }
+
+    /**
+     * Launch benchmark.
+     *
+     * @param args Args.
+     */
+    public static void main(String[] args) throws RunnerException {
+        ChainedOptionsBuilder builder = new OptionsBuilder()
+                .measurementIterations(20)
+                .operationsPerInvocation(3)
+                .warmupIterations(5)
+                .forks(1)
+                .threads(1)
+                .include(JmhStreamerAddDataBenchmark.class.getSimpleName());
+
+        new Runner(builder.build()).run();
+    }
+}
---
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/datastreamer/DataStreamerAddDataTest.java
@@ -0,0 +1,153 @@
+package org.apache.ignite.internal.processors.datastreamer;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.apache.ignite.Ignite;
+import org.apache.ignite.IgniteDataStreamer;
+import org.apache.ignite.cache.affinity.rendezvous.RendezvousAffinityFunction;
+import org.apache.ignite.configuration.CacheConfiguration;
+import org.apache.ignite.configuration.IgniteConfiguration;
+import org.apache.ignite.internal.util.typedef.internal.U;
+import org.apache.ignite.lang.IgniteFuture;
+import org.apache.ignite.lang.IgniteInClosure;
+import org.apache.ignite.testframework.GridStringLogger;
+import org.apache.ignite.testframework.junits.common.GridCommonAbstractTest;
+
+public class DataStreamerAddDataTest extends GridCommonAbstractTest {
+    static ExecutorService executor = Executors.newFixedThreadPool(1);
+
+    /** Config. */
+    private IgniteConfiguration cfg;
+
+    /** Time before. */
+    private long timeBefore;
+
+    /** Time after. */
+    private long timeAfter;
+
+    /** Entry amount. */
+    private static final Integer ENTRY_AMOUNT = 10;
+
+    /** String logger. */
+    private GridStringLogger strLog = new GridStringLogger();
+
+    /** Ignite data streamer. */
+    private static IgniteDataStreamer<Integer, Integer> dataLdr;
+
+    /** Client. */
+    private static Ignite client;
+
+    /** Server 1. */
+    private static Ignite srv1;
+
+    /** Server 2. */
+    private static Ignite srv2;
+
+    /** {@inheritDoc} */
+    @Override protected void beforeTestsStarted() throws Exception {
+        super.beforeTestsStarted();
+
+        srv1 = startGrid("server1");
+
+        srv2 = startGrid("server2");
+
+        client = startGrid("client");
+
+        dataLdr = client.dataStreamer(cfg.getCacheConfiguration()[0].getName());
+    }
+
+    /** {@inheritDoc} */
+    @Override protected void afterTestsStopped() throws Exception {
+        super.afterTestsStopped();
+
+        dataLdr.close();
+
+        stopAllGrids();
+    }
+
+    /** {@inheritDoc} */
+    @Override protected IgniteConfiguration getConfiguration(String igniteInstanceName) throws Exception {
+        cfg = super.getConfiguration(igniteInstanceName);
+
+        CacheConfiguration ccfg = defaultCacheConfiguration();
+
+        ccfg.setAffinity(new RendezvousAffinityFunction(false, 128));
+
+        if (igniteInstanceName.equals("client"))
+            cfg.setClientMode(true);
+
+        cfg.setCacheConfiguration(ccfg);
+
+        cfg.setGridLogger(strLog);
+
+        return cfg;
+    }
+
+    /**
+     *
+     */
+    public void testAddDataKeyValue() throws Exception {
+        int i = 0;
+
+        timeBefore = U.currentTimeMillis();
+        while (true) {
+                final int indx = i;
+                IgniteFuture igniteFuture = dataLdr.addData(i, i);
+                igniteFuture.listen(new IgniteInClosure<IgniteFuture<?>>() {
+                    @Override
+                    public void apply(IgniteFuture<?> igniteFuture) {
+                        igniteFuture.get();
+
+                        System.out.println("!!!~ done " + indx);
+                    }
+                });
+            igniteFuture.get();
+            i++;
+        }
+
+//        timeAfter = U.currentTimeMillis();
+
+//        System.out.println("Key/Value: " + (timeAfter - timeBefore));
+    }
+
+    /**
+     *
+     */
+    public void testAddDataCollection() throws Exception {
+        int i = 0;
+
+        while (true) {
+            Collection<java.util.AbstractMap.SimpleEntry<Integer, Integer>> testList = new ArrayList<>();
+
+            i++;
+            final int indx = i;
+            System.out.println(i );
+//            for (; i % ENTRY_AMOUNT != 0; i++)
+                testList.add(new HashMap.SimpleEntry<>(i, i));
+
+            timeBefore = U.currentTimeMillis();
+
+            IgniteFuture<?> igniteFuture = dataLdr.addData(testList);
+
+            timeAfter = U.currentTimeMillis();
+
+//            System.out.println("Collection: " + (timeAfter - timeBefore));
+            igniteFuture.listen(new IgniteInClosure<IgniteFuture<?>>() {
+                @Override
+                public void apply(IgniteFuture<?> igniteFuture) {
+                    igniteFuture.get();
+
+                    System.out.println("!!!~ done " + indx);
+                }
+            });
+        }
+//        dataLdr.flush();
+//        System.out.println(igniteFuture.get());
+    }
+}
