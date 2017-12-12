bd4de78f9ca717723a3cacd9af6f137a8229a526
1vanan <vanen31@gmail.com>
2017-12-12 11:09:26 +0300

    working process

--- modules/benchmarks/src/main/java/org/apache/ignite/internal/benchmarks/jmh/JmhStreamerAddDataBenchmark.java
+++ modules/benchmarks/src/main/java/org/apache/ignite/internal/benchmarks/jmh/JmhStreamerAddDataBenchmark.java
@@ -3,6 +3,17 @@ package org.apache.ignite.internal.benchmarks.jmh;
 import org.apache.ignite.Ignite;
 import org.apache.ignite.IgniteCache;
 import org.apache.ignite.IgniteDataStreamer;
+import org.apache.ignite.Ignition;
+import org.apache.ignite.cache.CacheAtomicityMode;
+import org.apache.ignite.cache.CacheMode;
+import org.apache.ignite.cache.CacheRebalanceMode;
+import org.apache.ignite.cache.CacheWriteSynchronizationMode;
+import org.apache.ignite.cache.affinity.rendezvous.RendezvousAffinityFunction;
+import org.apache.ignite.configuration.CacheConfiguration;
+import org.apache.ignite.configuration.IgniteConfiguration;
+import org.apache.ignite.configuration.NearCacheConfiguration;
+import org.apache.ignite.spi.discovery.tcp.TcpDiscoverySpi;
+import org.apache.ignite.spi.discovery.tcp.ipfinder.vm.TcpDiscoveryVmIpFinder;
 import org.openjdk.jmh.annotations.*;
 import org.openjdk.jmh.results.RunResult;
 import org.openjdk.jmh.runner.Runner;
@@ -16,60 +27,106 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.concurrent.TimeUnit;
 
+import static org.apache.ignite.cache.CacheAtomicityMode.TRANSACTIONAL;
+import static org.apache.ignite.cache.CacheWriteSynchronizationMode.FULL_SYNC;
+
 @State(Scope.Benchmark)
 public class JmhStreamerAddDataBenchmark extends JmhAbstractBenchmark {
-    /** Property: backups. */
+    /**
+     * Property: backups.
+     */
     protected static final String PROP_BACKUPS = "ignite.jmh.cache.backups";
 
-    /** Property: atomicity mode. */
+    /**
+     * Property: atomicity mode.
+     */
     protected static final String PROP_ATOMICITY_MODE = "ignite.jmh.cache.atomicityMode";
 
-    /** Property: atomicity mode. */
+    /**
+     * Property: atomicity mode.
+     */
     protected static final String PROP_WRITE_SYNC_MODE = "ignite.jmh.cache.writeSynchronizationMode";
 
-    /** Property: nodes count. */
+    /**
+     * Property: nodes count.
+     */
     protected static final String PROP_DATA_NODES = "ignite.jmh.cache.dataNodes";
 
-    /** Property: client mode flag. */
+    /**
+     * Property: client mode flag.
+     */
     protected static final String PROP_CLIENT_MODE = "ignite.jmh.cache.clientMode";
 
-    /** Default amount of nodes. */
+    /**
+     * Default amount of nodes.
+     */
     protected static final int DFLT_DATA_NODES = 1;
 
-    /** Client node. */
+    /** IP finder shared across nodes. */
+    private static final TcpDiscoveryVmIpFinder IP_FINDER = new TcpDiscoveryVmIpFinder(true);
+
+    /**
+     * Client node.
+     */
     protected Ignite clientNode;
 
-    /** First server node. */
+    /**
+     * First server node.
+     */
     protected Ignite svrNode1;
 
-    /** Server server node. */
+    /**
+     * Server server node.
+     */
     protected Ignite svrNode2;
 
-    /** Target cache. */
+    /**
+     * Target cache.
+     */
     protected IgniteCache cache;
 
-    /** Test list. */
+    /**
+     * Test list.
+     */
     private Collection<AbstractMap.SimpleEntry<Integer, Integer>> testList = new ArrayList<>();
 
 
-    /** Ignite data streamer. */
+    /**
+     * Ignite data streamer.
+     */
     private static IgniteDataStreamer<Integer, Integer> dataLdr;
+    private String DEFAULT_CACHE_NAME = "default";
 
     /**
      *
      */
     @Setup
-    public void setup(){
+    public void setup() {
 //        CacheConfiguration ccfg = new CacheConfiguration("default");
-//
+
 //        ccfg.setAtomicityMode(TRANSACTIONAL);
 //        ccfg.setNearConfiguration(new NearCacheConfiguration());
 //        ccfg.setWriteSynchronizationMode(FULL_SYNC);
 //        ccfg.setEvictionPolicy(null);
-//
+
 //        IgniteConfiguration cfgClient = new IgniteConfiguration();
+
+        IgniteConfiguration cfgSrv = new IgniteConfiguration();
+
+        IgniteConfiguration cfgSrv2 = new IgniteConfiguration();
+
 //
-//        IgniteConfiguration cfgSrv = new IgniteConfiguration();
+//        TcpDiscoverySpi spiSrv = (TcpDiscoverySpi) cfgSrv.getDiscoverySpi();
+//
+//        Collection collection = new ArrayList();
+//
+//        collection.add("127.0.0.1:47501");
+//
+//        TcpDiscoveryVmIpFinder ipFinder = new TcpDiscoveryVmIpFinder(true);
+//
+//        ipFinder.setAddresses(collection);
+//
+//        spiSrv.setIpFinder(ipFinder);
 //
 //        ccfg.setAffinity(new RendezvousAffinityFunction(false, 128));
 //
@@ -79,20 +136,23 @@ public class JmhStreamerAddDataBenchmark extends JmhAbstractBenchmark {
 //
 //        cfgClient.setClientMode(true);
 //
+//
+//
 //        clientNode = Ignition.start(cfgClient);
-//
-//        svrNode1 = Ignition.start(cfgSrv);
-//
-//        svrNode2 = Ignition.start(cfgSrv);
-//
+
+        svrNode1 = Ignition.start(cfgSrv);
+
+        svrNode2 = Ignition.start(cfgSrv2);
+
 //        dataLdr = clientNode.dataStreamer(ccfg.getName());
 
     }
+
     /**
      *
      */
     @Benchmark
-    public void addDataCollection(){
+    public void addDataCollection() {
         System.out.println(123);
 //        dataLdr.addData(testList);
     }
@@ -105,26 +165,65 @@ public class JmhStreamerAddDataBenchmark extends JmhAbstractBenchmark {
      */
     @TearDown
     public void tearDown() throws Exception {
-//        Ignition.stopAll(true);
+        Ignition.stopAll(true);
+    }
+
+    private static IgniteConfiguration getConfiguration(String instanceName){
+        IgniteConfiguration cfg = new IgniteConfiguration();
+
+        if(instanceName.contains("client"))
+            cfg.setClientMode(true);
+
+        cfg.setIgniteInstanceName(instanceName);
+
+        cfg.setLocalHost("127.0.0.1");
+
+        TcpDiscoverySpi discoSpi = new TcpDiscoverySpi();
+        discoSpi.setIpFinder(IP_FINDER);
+        cfg.setDiscoverySpi(discoSpi);
+
+        cfg.setCacheConfiguration(cacheConfiguration());
+
+        return cfg;
+    }
+
+    protected CacheConfiguration cacheConfiguration() {
+        CacheConfiguration cacheCfg = new CacheConfiguration();
+
+        cacheCfg.setName(DEFAULT_CACHE_NAME);
+        cacheCfg.setCacheMode(CacheMode.PARTITIONED);
+        cacheCfg.setRebalanceMode(CacheRebalanceMode.SYNC);
+
+        // Set atomicity mode.
+        CacheAtomicityMode atomicityMode = enumProperty(PROP_ATOMICITY_MODE, CacheAtomicityMode.class);
+
+        if (atomicityMode != null)
+            cacheCfg.setAtomicityMode(atomicityMode);
+
+        // Set write synchronization mode.
+        CacheWriteSynchronizationMode writeSyncMode =
+                enumProperty(PROP_WRITE_SYNC_MODE, CacheWriteSynchronizationMode.class);
+
+        if (writeSyncMode != null)
+            cacheCfg.setWriteSynchronizationMode(writeSyncMode);
+
+        // Set backups.
+        cacheCfg.setBackups(intProperty(PROP_BACKUPS));
+
+        return cacheCfg;
     }
 
     public static void main(String[] args) throws RunnerException {
         ChainedOptionsBuilder builder = new OptionsBuilder()
-//                .jvmArgs()
-                .timeUnit(TimeUnit.MILLISECONDS)
-                .measurementIterations(10)
-                .measurementTime(TimeValue.seconds(20))
-//                .warmupIterations(5)
-//                .warmupTime(TimeValue.seconds(10))
+                .timeUnit(TimeUnit.NANOSECONDS)
+                .measurementIterations(1)
+                .measurementTime(TimeValue.seconds(1))
+                .warmupIterations(1)
                 .forks(1)
-                .threads(1)
-                .mode(Mode.Throughput)
+                .threads(10)
+                .mode(Mode.AverageTime)
                 .include(JmhStreamerAddDataBenchmark.class.getSimpleName());
 
-        Collection<RunResult> runResult = new Runner(builder.build()).run();
-        for (RunResult r:
-             runResult)
-            System.out.println("RESULT: " + r.getPrimaryResult().toString());
-
+        new Runner(builder.build()).run();
     }
 }
